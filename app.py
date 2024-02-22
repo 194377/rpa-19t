b66b01b7989264356362d1716e99907c4b2767e7
Fanjie Guo <160450779+194377@users.noreply.github.com>
2024-02-22 11:30:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ from flask import Flask,request,render_template
 import replicate
 import os
 import time
-from OpenAI import OpenAI
 
-model = OpenAI(api_key="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9")
-os.environment["REPLICATE_API_TOKEN"] = "r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
-
-model = OpenAI(api_key=openapi_api_key)
+os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
 
 app = Flask(__name__)
 
@@ -25,6 +21,15 @@ def main():
         r = request.form.get("r")
         first_time=0
     return(render_template("main.html",r=r))
+
+@app.route("/text_gpt",methods=["GET","POST"])
+def text_gpt():
+    return(render_template("text_gpt.html"))
+
+@app.route("/text_result",methods=["GET","POST"])
+def text_result():
+    q = request.form.get("q")
+    return(render_template("text_result.html",r="API not ready"))
 
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
@@ -44,9 +49,9 @@ def image_result():
 
 @app.route("/end",methods=["GET","POST"])
 def end():
-    global first_time
+    global first_time,r
     first_time = 1
-    return(render_template("end.html"))
+    return(render_template("end.html",r=r))
 
 if __name__ == "__main__":
     app.run()
